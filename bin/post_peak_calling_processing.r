c36ee0f7c71e438c4e2c497668bb11d4892ef3be
Lucia Conde <l.conde@ucl.ac.uk>
2019-02-08 21:16:38 +0000

    fix chippeakanno

--- bin/post_peak_calling_processing.r
+++ bin/post_peak_calling_processing.r
@@ -12,6 +12,8 @@ Blacklist <- as.character(args[3])
 GTF <- as.character(args[4])
 input <- as.character(args[5:length(args)])
 
+
+
 # Load / install required packages
 .libPaths(c(R_lib, .libPaths()))
 
@@ -51,6 +53,15 @@ cores <- detectCores() / 2
 registerDoParallel(cores)
 
 annotation <- as.data.frame(gtf)
+
+###################################################################
+###################################################################
+## LC EDITS
+###################################################################
+annotation <- annotation[annotation$type == 'CDS',]
+###################################################################
+###################################################################
+
 annotation <- annotation[!duplicated(annotation), ]
 annotation <- makeGRangesFromDataFrame(annotation, keep.extra.columns = T)
 
@@ -60,6 +71,31 @@ annotation <- makeGRangesFromDataFrame(annotation, keep.extra.columns = T)
 #- some gene_ids are NA, so add na.omit
 #- not all the gft's have gene_name, so correct that
 #- replace '== x' with '%in% x' or it crashes
+#- remove the "gap" correction, as it was making a mess for genes mapped in different chrs
+#	> df
+#	  seqnames     start       end width strand   symbol  gene_id
+#	1     chr2  36389911  36389995    85      + Mir684-1 Mir684-1
+#	2     chr2  80628705  80628790    86      - Mir684-1 Mir684-1
+#	3     chr5  24257528  24257612    85      + Mir684-1 Mir684-1
+#	4     chr7 127398916 127399000    85      + Mir684-1 Mir684-1
+#	5    chr10 129665755 129665839    85      + Mir684-1 Mir684-1
+#	6    chr11  75065907  75065991    85      - Mir684-1 Mir684-1
+#	7    chr11 115523132 115523216    85      - Mir684-1 Mir684-1
+#	8    chr16  20176150  20176234    85      - Mir684-1 Mir684-1
+#	9     chrX   7753089   7753173    85      + Mir684-1 Mir684-1
+#	> df$start <- min(df$start, na.rm=T)
+#	> df$end <- max(df$end, na.rm=T)
+#	> df
+#	  seqnames   start       end width strand   symbol  gene_id
+#	1     chr2 7753089 129665839    85      + Mir684-1 Mir684-1
+#	2     chr2 7753089 129665839    86      - Mir684-1 Mir684-1
+#	3     chr5 7753089 129665839    85      + Mir684-1 Mir684-1
+#	4     chr7 7753089 129665839    85      + Mir684-1 Mir684-1
+#	5    chr10 7753089 129665839    85      + Mir684-1 Mir684-1
+#	6    chr11 7753089 129665839    85      - Mir684-1 Mir684-1
+#	7    chr11 7753089 129665839    85      - Mir684-1 Mir684-1
+#	8    chr16 7753089 129665839    85      - Mir684-1 Mir684-1
+#	9     chrX 7753089 129665839    85      + Mir684-1 Mir684-1
 ###################################################################
 #anno <- plyr::adply(unique(annotation$gene_id),
 #                    .margins = 1,
@@ -84,9 +120,6 @@ anno <- plyr::adply(unique(na.omit(annotation$gene_id)),
 			}
                         aux$gene_id <- x
                         df <- as.data.frame(aux)
-                        # For some ENSG genes, there is a small gap between transcripts
-                        df$start <- min(df$start, na.rm=T)
-                        df$end <- max(df$end, na.rm=T)
                         df
                     }, .id = NULL, .parallel = TRUE)
 ###################################################################
@@ -96,16 +129,33 @@ anno <- plyr::adply(unique(na.omit(annotation$gene_id)),
 annoData <- unique(makeGRangesFromDataFrame(anno, keep.extra.columns = T))
 names(annoData) <- annoData$gene_id
 
-# Read in blacklist file and convert into range object
+
+###################################################################
+###################################################################
+## LC EDITS
+#- keep the 'chr'
+###################################################################
+## Read in blacklist file and convert into range object
+#if (Blacklist != "No-filtering") {
+#    blacklist <- read.table(Blacklist, header = FALSE)
+#    blacklist_range <- with(blacklist, 
+#                            GRanges(sub("chr", "", V1), 
+#                                    IRanges(start = V2, end = V3), 
+#                                    strand = Rle(rep("+", nrow(blacklist)))
+#                                    )
+#                            )
+#}
 if (Blacklist != "No-filtering") {
     blacklist <- read.table(Blacklist, header = FALSE)
     blacklist_range <- with(blacklist, 
-                            GRanges(sub("chr", "", V1), 
+                            GRanges(V1, 
                                     IRanges(start = V2, end = V3), 
                                     strand = Rle(rep("+", nrow(blacklist)))
                                     )
                             )
 }
+###################################################################
+###################################################################
 
 # Process output files from MACS: filtering peaks that overlap with blacklisted regions and annotating peaks
 for (i in 1:length(input)) {
@@ -153,16 +203,26 @@ for (i in 1:length(input)) {
 
     write.table(df, file = newfilename, quote = F, sep = "\t", row.names = F, col.names = F)
 
-    # Annotation
+
+   # Annotation
     final_anno <- annotatePeakInBatch(
                                       final,
                                       AnnotationData = annoData,
                                       output = "overlapping",
                                       maxgap = 5000L
                                       )
-
-    # Write annotated peaks to file
-    final_anno_df <- as.data.frame(final_anno)
+ 
+###################################################################
+###################################################################
+## LC EDITS
+#- add rownmaes = null or it fails when the rownmaes (gene_ids) are
+#-	not unique (happens with multiple CDSs for the same gene int he gft
+###################################################################
+#    # Write annotated peaks to file
+#    final_anno_df <- as.data.frame(final_anno)
+   final_anno_df <- as.data.frame(final_anno, row.names=NULL)
+###################################################################
+###################################################################
     final_anno_df$gene_symbol <- anno[match(final_anno_df$feature, anno$gene), ]$symbol
 
     newfilename <- paste(sub("_peaks.xls", "", basename(input[i])), filter_flag, "_annotated.txt", sep = "")
